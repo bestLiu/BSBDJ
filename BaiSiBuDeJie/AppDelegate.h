e0988328993d894db76ad8a965c3a8cf4bd6d17d
mac1 <cyjjkz1@gmail.com>
2016-02-15 14:46:52 +0800

    初始化项目

---
+++ BaiSiBuDeJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiSiBuDeJie
+//
+//  Created by mac1 on 16/2/15.
+//  Copyright © 2016年 BNDK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaiSiBuDeJie/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BaiSiBuDeJie
+//
+//  Created by mac1 on 16/2/15.
+//  Copyright © 2016年 BNDK. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
